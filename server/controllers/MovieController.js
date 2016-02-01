8da102ae50cbfa8cfd2fab6108de4e6eb8d51b81
Danis Lee <lee@Daniss-MacBook-Pro.local>
2016-02-01 15:46:57 +0300

    add to database error

---
+++ server/controllers/MovieController.js
@@ -0,0 +1,20 @@
+  var restful = require('node-restful');
+
+    module.exports = function(app, route) {
+
+      // Setup the controller for REST;
+    var rest = restful.model(
+    'movie',
+    app.models.movie
+  ).methods(['get', 'put', 'post', 'delete']);
+
+    //register this endpoint with application
+    rest.register(app, route);
+
+      // Return middleware.
+      return function(req, res, next) {
+       next();
+      };
+
+    };
+    console.log('works clear');
